85320c5e9382eaa824f2efaffdd1619020e2273e
dolphinxyz <robimalco@gmail.com>
2021-10-18 20:46:31 +0200

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="flex h-screen">
+    <div class="m-auto">
+      <span class="text-6xl font-bold">Hi, I am Dolphin</span><br>
+      <span class="text-2xl font-thin">Coming Soon</span>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+@Component({})
 export default class Home extends Vue {}
 </script>
